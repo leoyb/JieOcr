87cb7cf150fc2eeef27efca6b0eeb962c30d11c4
leoyb <917604648@qq.com>
2023-12-06 15:39:47 +0800

    代码优化

--- src/main/java/ocr/sdk/OcrRecognition.java
+++ src/main/java/ocr/sdk/OcrRecognition.java
@@ -1,11 +1,9 @@
 package ocr.sdk;
 
-import ai.djl.MalformedModelException;
 import ai.djl.modality.cv.Image;
 import ai.djl.modality.cv.output.DetectedObjects;
 import ai.djl.modality.cv.output.Rectangle;
 import ai.djl.repository.zoo.Criteria;
-import ai.djl.repository.zoo.ModelNotFoundException;
 import ai.djl.repository.zoo.ModelZoo;
 import ai.djl.repository.zoo.ZooModel;
 import ai.djl.training.util.ProgressBar;
@@ -14,7 +12,6 @@ import ocr.sdk.recognition.PpWordRecognitionTranslator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
@@ -37,9 +34,6 @@ public class OcrRecognition {
     static final RejectedExecutionHandler policy = new ThreadPoolExecutor.DiscardPolicy();
     static final ThreadPoolExecutor executorService = new ThreadPoolExecutor(poolSize, poolSize, 0, TimeUnit.SECONDS, queue, policy);
 
-    static final ZooModel detectionModel;
-    static final ZooModel recognitionModel;
-
 
     //1.识别大奖
     static final Rectangle rectangle0 = new Rectangle(0, 0.5, 1, 0.4);
@@ -50,18 +44,12 @@ public class OcrRecognition {
     static final Rectangle rectangle4 = new Rectangle(0.86, 0.945, 0.043, 0.054);
 
 
-    static {
-        try {
-            //只需要加载一次，不用每次都加载
-            detectionModel = ModelZoo.loadModel(Criteria.builder().optEngine("PaddlePaddle").optModelName("inference").setTypes(Image.class, DetectedObjects.class).optModelPath(Paths.get("src/models/ch_PP-OCRv3_det_infer")).optTranslator(new PpWordDetectionTranslator()).optProgress(new ProgressBar()).build());
-            recognitionModel = ModelZoo.loadModel(Criteria.builder().optEngine("PaddlePaddle").optModelName("inference").setTypes(Image.class, String.class).optModelPath(Paths.get("src/models/ch_PP-OCRv3_rec_infer")).optProgress(new ProgressBar()).optTranslator(new PpWordRecognitionTranslator()).build());
-        } catch (IOException | ModelNotFoundException | MalformedModelException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
 
     public List<DetectedObjects.DetectedObject> recognizeImg(Image image) throws Exception {
+
+        ZooModel detectionModel = ModelZoo.loadModel(Criteria.builder().optEngine("PaddlePaddle").optModelName("inference").setTypes(Image.class, DetectedObjects.class).optModelPath(Paths.get("src/models/ch_PP-OCRv3_det_infer")).optTranslator(new PpWordDetectionTranslator()).optProgress(new ProgressBar()).build());
+        ZooModel  recognitionModel = ModelZoo.loadModel(Criteria.builder().optEngine("PaddlePaddle").optModelName("inference").setTypes(Image.class, String.class).optModelPath(Paths.get("src/models/ch_PP-OCRv3_rec_infer")).optProgress(new ProgressBar()).optTranslator(new PpWordRecognitionTranslator()).build());
+
         List<DetectedObjects.DetectedObject> results = new ArrayList<>();
         List<DetectedObjects.DetectedObject> result = new ImgTask(ImgTask.price, image, rectangle0, detectionModel, recognitionModel).call();
         results.addAll(result);
@@ -74,6 +62,8 @@ public class OcrRecognition {
         result = new ImgTask(ImgTask.score, image, rectangle4, detectionModel, recognitionModel).call();
         results.addAll(result);
 
+        detectionModel.close();
+        recognitionModel.close();
         return results;
     }
 
